d568c925164c058c07cbec46445db63afaf40f84
nukkueno <unukubodiamar@gmail.com>
2025-02-16 14:27:43 +0800

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  modules: ["vuetify-nuxt-module", "@pinia/nuxt"],
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: false },
+  runtimeConfig: {
+    public: {
+      baseURL: "http://localhost:4004/api/v1",
+    },
+  },
+  devServer: {
+    port: 3000,
+  },
+});
